ebf12735294cd418bfa993ae6e5e41d9cb02d24d
Dunstan M'mbehero <dunmmbehero@gmail.com>
2021-04-18 12:23:58 +0300

    update main init file

--- app/main/__init__.py
+++ app/main/__init__.py
@@ -1,11 +1,3 @@
-from flask import Flask
-from app import app
-
-app = Flask(__name__)
-
-@app.route('/')
-def index():
-    return "Hello, world!"
-
-if __name__ == "__main__":
-    app.run(debug=True)
+from flask import Blueprint
+main = Blueprint('main',__name__)
+from . import views,errors
