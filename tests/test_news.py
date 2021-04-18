b4c798ab35437ad805e80a310d59386bb89e5671
Dunstan M'mbehero <dunmmbehero@gmail.com>
2021-04-18 14:39:42 +0300

    update readme

--- tests/test_news.py
+++ tests/test_news.py
@@ -33,16 +33,16 @@ class ArticlesTest(unittest.TestCase):
         '''
         Set up method that will run before every Test
         '''
-        self.new_article = Articles('CNN','Peter Polle','The tech scene in Africa-Is it the next big thing?','A look at various tech hubs in Africa and the impact they have on the worlds economy','techie.com','techie.com/7643t94.jpg','2018-04-11T07:57:16Z')
+        self.new_article = Articles('CNN','David Goldman','What is Dogecoin? How a joke became hotter than bitcoin','cnn.com','https://cdn.cnn.com/cnnnext/dam/assets/210107104229-03-bitcoin---stock-super-tease.jpg',' April 17, 2021')
 
     def test_instance(self):
         self.assertTrue(isinstance(self.new_article,Articles))
 
     def test_to_check_instance_variables(self):
         self.assertEquals(self.new_article.id,'CNN')
-        self.assertEquals(self.new_article.author,'Peter Polle')
-        self.assertEquals(self.new_article.title,'The tech scene in Africa-Is it the next big thing?')
-        self.assertEquals(self.new_article.description,'A look at various tech hubs in Africa and the impact they have on the worlds economy')
-        self.assertEquals(self.new_article.url,'techie.com')
-        self.assertEquals(self.new_article.image,'techie.com/7643t94.jpg')
-        self.assertEquals(self.new_article.date,'2018-04-11T07:57:16Z')
+        self.assertEquals(self.new_article.author,'David Goldman')
+        self.assertEquals(self.new_article.title,'What is Dogecoin? How a joke became hotter than bitcoin')
+        self.assertEquals(self.new_article.description,'a look at crypto ')
+        self.assertEquals(self.new_article.url,'https://edition.cnn.com/2021/04/17/investing/what-is-dogecoin/index.html')
+        self.assertEquals(self.new_article.image,'https://cdn.cnn.com/cnnnext/dam/assets/210107104229-03-bitcoin---stock-super-tease.jpg')
+        self.assertEquals(self.new_article.date,'April 17, 2021')
